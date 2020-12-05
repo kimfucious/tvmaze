866964475281b4d3c7067acfc7eba58dbc415bed
Kimfucious <kimfucious@abts.io>
2020-12-05 21:42:21 +0700

    fix: remove onClick for episode

--- src/components/EpidodeListItem.js
+++ src/components/EpidodeListItem.js
@@ -1,26 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { EpisodeMarker } from "./EpisodeMarker";
-import { useDispatch } from "react-redux";
 import dayjs from "dayjs";
 
 export const EpisodeListItem = ({ episode: item }) => {
-  const dispatch = useDispatch();
-
-  const handleSelectShow = async () => {
-    try {
-      dispatch({ type: "SET_SELECTED_SHOW", payload: item });
-    } catch (error) {
-      console.warn(error);
-    }
-  };
-
   return (
-    <div
-      href="#"
-      className="d-flex mb-3 p-2 animate__animated animate__fadeIn"
-      onClick={() => handleSelectShow()}
-    >
+    <div href="#" className="d-flex mb-3 p-2 animate__animated animate__fadeIn">
       <EpisodeMarker episodeNumber={item.number} />
       <div className="d-flex flex-column justify-content-center ml-3">
         <div className="lead font-weight-bold" style={{ fontSize: 18 }}>
